6f972eaf1b920cea2d44d2913113a7d1db59ee2e
flogy <gygy1992@hotmail.com>
2020-05-19 07:40:49 +0200

    use Capacitor native API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
 import React from "react";
+import { Plugins } from "@capacitor/core";
 
-export default () => <h1>Hello world</h1>;
+export default () => {
+  const displayNativeToast = async () => {
+    await Plugins.Toast.show({
+      text: "Hello world",
+    });
+  };
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <button onClick={displayNativeToast}>Display native toast!</button>
+    </>
+  );
+};

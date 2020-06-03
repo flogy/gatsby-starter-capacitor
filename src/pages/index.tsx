bb0207c5259a80f1d3309201f80c8d0d39bb3b3f
Florian Gyger <gygy1992@hotmail.com>
2020-06-03 16:11:08 +0200

    improved look of index page for Gatsby starter library preview image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Plugins } from "@capacitor/core";
+import gatsbyStartercapacitor from "../assets/gatsby-starter-capacitor.png";
 
 export default () => {
   const displayNativeToast = async () => {
@@ -9,9 +10,16 @@ export default () => {
   };
 
   return (
-    <>
-      <h1>Hello world</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>gatsby-starter-capacitor</h1>
+      <img src={gatsbyStartercapacitor} alt="Gatsby and Capacitor" />
       <button onClick={displayNativeToast}>Display native toast!</button>
-    </>
+    </div>
   );
 };
